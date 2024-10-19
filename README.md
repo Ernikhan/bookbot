1da71185853739f5ba4de9518828118cc3e7ef65
edv_esdras <ernesto.d.v@live.com>
2024-10-18 21:11:06 -0600

    B: Updated comments on main.py and Readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # bookbot
 
 BookBot is my first project!
+
+Proyecto implementando conocimientos de:
+* Python
+* Shells and Terminals
+* Git and GitHub
+* Extra Python:
+    + Methods for file objects
+    + Sort Method in dictionaries
--- main.py
+++ main.py
@@ -14,17 +14,19 @@ def main():
     
 
 
-
+# funcion para abrir el archivo y regresar el file object con read
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 
+# función que separa el texto a una lista con cada palabra y realiza un coteo total con el lentgh de la lista
 def count_words(text):
     words = text.split()
     return len(words)
 
 
+# función que realiza un conteo de cada letra en el texto y lo inserta en un diccionario
 def count_characters(text):
     lower_text = text.lower() #convierte todo caracter en el string en minuscula
     char_dictionary = {"a":0,"b":0,"c":0,"d":0,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,"o":0,"p":0,"q":0,"r":0,"s":0,"t":0,"u":0,"v":0,"w":0,"x":0,"y":0,"z":0}
@@ -34,6 +36,7 @@ def count_characters(text):
     return char_dictionary
  
 
+# función que crea una lista de diccionarios usando el diccionario de letras y conteo como imput. Enlista las letras de mayor a menor ocurrencia.
 def sortdir_tolist(dicto):
     sorted_list = []
     for character in dicto:
